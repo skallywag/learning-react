23a24a300b17b54b7ccbb61c9eb48cabc38d11b0
Samuel Lee <lee.sam1492@gmail.com>
2022-01-21 17:11:51 -0700

    learning!

--- src/index.js
+++ src/index.js
@@ -10,26 +10,56 @@ import "./index.css";
 // className instead of class
 // close every element(self close)
 
+// setup variable
+const books = [
+  {
+    id: 1,
+    img: "https://images-na.ssl-images-amazon.com/images/I/91vnzZO5yPL._AC_UL200_SR200,200_.jpg",
+    title: "The very hungry caterpillar",
+    author: "Eric Carle",
+  },
+  {
+    id: 2,
+    img: "https://images-na.ssl-images-amazon.com/images/I/51o4b5AdNLL._AC_UL200_SR200,200_.jpg",
+    title: "Wheres Spot?",
+    author: "Eric Hill",
+  },
+  {
+    id: 3,
+    img: "https://images-na.ssl-images-amazon.com/images/I/81PUnVR%2BowL._AC_UL200_SR200,200_.jpg",
+    title: "Jhonny the walrus",
+    author: "Matt Walsh",
+  },
+];
+
 function BookList() {
   return (
     <section className="booklist">
-      <Book />
+      {books.map((book) => {
+        return <Book key={book.id} book={book}></Book>;
+      })}
     </section>
   );
 }
 
-const author = "Eric Carle";
-const Book = () => {
-  const title = "The very hungry caterpillar";
+const Book = (props) => {
+  // events:
+  // attribute, eventHandler
+  // onClick, onMouseOver
+  const { img, title, author } = props.book;
+
+  const clickMe = () => {
+    alert("you clicked me!");
+  };
+
   return (
     <article className="book">
-      <img
-        src="https://images-na.ssl-images-amazon.com/images/I/91vnzZO5yPL._AC_UL200_SR200,200_.jpg"
-        alt=""
-      />
+      <img src={img} alt="" />
       <h1>{title}</h1>
-      <h4>{author.toLocaleUpperCase()}</h4>
-      {/* <p>{let x = 6}</p> */}
+      <h4>{author}</h4>
+      <button type="button" onClick={clickMe}>
+        click me
+      </button>
     </article>
   );
 };
